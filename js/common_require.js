ed5d33f55c9126b50fdc5ee7306c0cb3e25f67de
Ryuichi Nonaka <nonaka.ryuichi@classmethod.jp>
2013-03-28 17:21:13 +0900

    add LAB.js sample

---
+++ js/common_require.js
@@ -0,0 +1,27 @@
+requirejs.config({
+    paths: {
+        "jquery": "jquery-1.9.1.min",
+        "jquery.easing": "jquery.easing.1.3",
+        "jquery.mousewheel": "jquery.mousewheel",
+        "jquery.bootstrap": "bootstrap.min",
+        "quo": "quo"
+    },
+    shim: {
+        "jquery.bootstrap": {
+            deps: ["jquery"]
+        },
+        "jquery.easing": {
+            desp: ["jquery"]
+        },
+        "jquery.mousewheel": {
+            desp: ["jquery"]
+        },
+        "quo": {
+            desp: ["jquery"]
+        }
+    }
+});
+
+require(["jquery", "jquery.easing", "jquery.mousewheel", "jquery.bootstrap"],function(){
+
+});
